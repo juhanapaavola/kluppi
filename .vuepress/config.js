bb290053aa40e2ab26af856b44780ac5298a8591
juhana.paavola <juhana.paavola@gmail.com>
2019-07-26 17:07:13 +0300

    google analytics to head

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -17,10 +17,12 @@ module.exports = {
         'var _iub = _iub || [];_iub.csConfiguration = {"lang":"en","siteId":1636663,"cookiePolicyId":85215248, "banner":{ "position":"float-bottom-center" }};'
         ],
         ['script', { src: "http://cdn.iubenda.com/cs/iubenda_cs.js"}],
-        
-        
+        ['script', { src: "https://www.googletagmanager.com/gtag/js?id=UA-144246311-2"}],
+        ['script', {},
+        "window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'UA-144246311-2');"
+        ]
     ],
-    plugins: {
+    plugins: {      
         'seo': { 
             siteTitle: (_, $site) => $site.title,
             title: $page => $page.title,
@@ -33,6 +35,6 @@ module.exports = {
             image: ($page, $site) => $page.frontmatter.image && (($site.themeConfig.domain || '') + $page.frontmatter.image),
             publishedAt: $page => $page.frontmatter.date && new Date($page.frontmatter.date),
             modifiedAt: $page => $page.lastUpdated && new Date($page.lastUpdated),
-        }
+        },
     },
 }
